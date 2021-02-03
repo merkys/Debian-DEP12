e392c00ef3c6c0d5c546fa8f512eb187063d5418
Andrius Merkys <andrius.merkys@gmail.com>
2021-02-03 15:40:59 +0200

    Validating URLs.

--- lib/Debian/DEP12.pm
+++ lib/Debian/DEP12.pm
@@ -3,9 +3,14 @@ package Debian::DEP12;
 use strict;
 use warnings;
 
+# ABSTRACT: validator for DEP 12 format
+# VERSION
+
+use Data::Validate::URI qw( is_uri );
 use Text::BibTeX::Validate qw( validate_BibTeX );
 
-sub _to_BibTeX {
+sub _to_BibTeX
+{
     my( $self ) = @_;
 
     my @BibTeX;
@@ -16,8 +21,22 @@ sub _to_BibTeX {
     return @BibTeX;
 }
 
-sub validate {
+sub validate
+{
     my( $self ) = @_;
+
+    # TODO: validate other fields
+
+    for my $key ('Bug-Database', 'Bug-Submit', 'Documentation',
+                 'Donation', 'FAQ', 'Gallery', 'Other-References',
+                 'Registration', 'Repository', 'Repository-Browse',
+                 'Webservice') {
+        next if !exists $self->{$key};
+        next if defined is_uri $self->{$key};
+        warn sprintf '%s: value \'%s\' does not look like valid URL' . "\n",
+                     $key,
+                     $self->{$key};
+    }
 
     for my $BibTeX ($self->_to_BibTeX) {
         validate_BibTeX( $BibTeX );
